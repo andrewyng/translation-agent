b61b873e005518530424378517d43c23a4f8ae41
Shaoyong Shen <shaoyong.shen@gmail.com>
2024-06-23 13:47:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import translation_agent as ta
 source_text = "Today is a good day. Sunny, shine, breeze, blue sky"
-source_lang, target_lang, country = "English", "Simplified Chinese", "China"
-translation = ta.translate(source_lang, target_lang, source_text, country)
+source_lang, target_lang, country,max_tokens = "English", "Simplified Chinese", "China", 10
+translation = ta.translate(source_lang, target_lang, source_text, country,max_tokens=max_tokens)
